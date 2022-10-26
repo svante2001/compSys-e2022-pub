f23777210ed102ed93d192755f826073c320eb5a
svante2001 <svante2001@gmail.com>
2022-10-26 17:33:25 +0200

    Server exercise

--- lectures/221026_network_programming/code_echo/echoserver.c
+++ lectures/221026_network_programming/code_echo/echoserver.c
@@ -11,11 +11,39 @@ void echo(int connfd)
     rio_t rio;
 
     Rio_readinitb(&rio, connfd);
-    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
-		printf("server received %d bytes\n", (int)n);
-		Rio_writen(connfd, buf, n);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:
+      printf("server received %d bytes\n", (int)n);
+
+      if ((int)n == 5 && !strcmp(buf, "ping\n")) { // check size and value.
+        Rio_writen(connfd, "pong\n", 5);
+      } else {
+	Rio_writen(connfd, buf, n);
+      }	      
     }
 }
+
+void store(int connfd) {
+  size_t n;
+  char buf[MAXLINE];
+  char db [MAXLINE];
+  rio_t rio;
+  
+  Rio_readinitb(&rio, connfd);
+
+  while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:
+    printf("server received %d bytes\n", (int)n);
+
+    if (!strncmp(buf, "PUT", 3)) {
+      strcpy(db, &buf[4]);
+      Rio_writen(connfd, "Wrote in string.\n", 17);
+    } else if (!strncmp(buf, "GET\n", 4)) {
+      Rio_writen(connfd, db, strlen(db));
+    } else {
+      Rio_writen(connfd, "Not a valid request\n", 20);
+    }
+  } 
+}
+
 
 int main(int argc, char **argv)
 {
@@ -34,10 +62,13 @@ int main(int argc, char **argv)
         clientlen = sizeof(struct sockaddr_storage);
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE,
-                    client_port, MAXLINE, 0);
+                   client_port, MAXLINE, 0);
         printf("Connected to (%s, %s)\n", client_hostname, client_port);
-        echo(connfd);
-        Close(connfd);
+
+	//echo(connfd);
+	store(connfd);
+
+	Close(connfd);
     }
     exit(0);
 }
